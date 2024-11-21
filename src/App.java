77fd623f5e13451fa1a3e64d313fad57e0c05384
Joe Weider Berbesi Chapeta <joewbch@gmail.com>
2024-11-21 17:58:03 -0500

    creacion metodo menu

--- src/App.java
+++ src/App.java
@@ -4,4 +4,18 @@ public class App {
         System.out.println("Lo mejor del mundo");
     }
     
+    public void menu(){
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("-----------------Bienvenido a Galaxy Express-------------------");
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("-Su mejor opcion a la hora de realizar viajes interplanetarios-");
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("\nPor favor, seleccione su planeta de destino: ");
+        System.out.println("-------------------------------------------");
+        System.out.println("-MENU DE PLANETAS-");
+        System.out.println("1.Mercurio\n2.Venus\n3.Olimpo\n4.Marte\n5.Júpiter\n6.Saturno\n7.Urano\n8.Neptuno");
+        System.out.println("-------------------------------------------");
+    }
+
+    
 }
