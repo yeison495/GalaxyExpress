ae7c01e6de2389484fe3c458171627c24224e6ba
Joe Weider Berbesi Chapeta <joewbch@gmail.com>
2024-11-21 21:47:36 -0500

    creacion metodo planetMap

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
+
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Lo mejor del mundo");
+        App app = new App();
+        app.menu();
+        System.out.println(app.planetMap());
         System.out.println("Lo mejor del mundo");
     }
     
@@ -13,9 +22,19 @@ public class App {
         System.out.println("\nPor favor, seleccione su planeta de destino: ");
         System.out.println("-------------------------------------------");
         System.out.println("-MENU DE PLANETAS-");
-        System.out.println("1.Mercurio\n2.Venus\n3.Olimpo\n4.Marte\n5.Júpiter\n6.Saturno\n7.Urano\n8.Neptuno");
+        System.out.println("1.Mercurio\n2.Venus\n3.Marte\n4.Júpiter\n5.Saturno\n6.Urano\n7.Neptuno");
         System.out.println("-------------------------------------------");
     }
 
-    
+    public Map planetMap(){
+        Map <String, Integer> planetMap = new HashMap<>();
+        planetMap.put("Mercurio", 222);
+        planetMap.put("Venus", 261);
+        planetMap.put("Marte", 401);
+        planetMap.put("Júpiter", 968);
+        planetMap.put("Saturno", 1660);
+        planetMap.put("Urano", 3151);
+        planetMap.put("Neptuno", 4700);
+        return planetMap;
+    }
 }
