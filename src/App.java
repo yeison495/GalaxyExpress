e7867c2c68bd673f7df70708b3ac0ad5a05df666
Joe Weider Berbesi Chapeta <joewbch@gmail.com>
2024-11-22 18:28:18 -0500

    ajustes metodos y agregacion de metodos de visualizar mapas y creacion de mapas

--- src/App.java
+++ src/App.java
@@ -1,156 +1,172 @@
+// librerias importadas
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+// simulador de viaje intergalactivo GalaxyExpress
 
 public class App {
     // variable scanner global
     static Scanner sc = new Scanner(System.in);
     // metodo principal
     public static void main(String[] args) throws Exception {
-        // inicio de simulador
-        App app = new App();
+        // inicio de simulador        
         System.out.println("---------------------------------------------------------------");
         System.out.println("-----------------Bienvenido a Galaxy Express-------------------");
         System.out.println("---------------------------------------------------------------");
         System.out.println("-Su mejor opcion a la hora de realizar viajes interplanetarios-");
         System.out.println("---------------------------------------------------------------");
         // creacion e inicializacion de variables
-        var select=0;
-        var pla=0;
-        var nav=0;
-        // ciclo para la seleccion de menu
+        var select = 0;
+        // creacion de mapas
+        Map<String, Integer> pla = planetMap();
+        Map<String, Integer> nav = spaceshipMap();
+        boolean ok = false;
+        // ciclo para la verificar errores        
         do {
-            select=app.menu();
-            // condicional de ejecucion
-            if(select==1){
-                pla=app.planetMap();      
-                System.out.println(pla);         
-            }else if (select==2) {
-                nav=app.naveMap();      
-                System.out.println(nav); 
-            }else if (select==3) {
+            try {
+                select = menu();
+                ok = true;
+            } catch (Exception e) {
+                System.out.println("La opción debe ser numerica...");
+                sc.nextLine();
+            }
+        } while (!ok);
+
+        switch (select) {
+            case 1 ->
+                viewPlanet();
+            case 2 ->
+                viewSpaceship();
+            case 3 ->
+                managerTrip(pla, nav);
+            case 4 ->
                 System.out.println();
-            }
-            else if (select==4) {
-                System.out.println("Bye");
-            }
-            else{
-                System.out.println("Seleccion invalida");  
-            }                        
-        } while (select!=4);        
-        
+            case 5 ->
+                System.out.println("\n*Al infinito y mas allá...BYE*\n");
+            default ->
+                System.out.println("\nSeleccion invalida\n");
+        }
+        System.out.println("*GEYELQUELOLEA*");
     }
-    // metodo para menu
-    public Integer menu(){        
-        System.out.println("\n-MENU-");
-        System.out.println("\n1.Seleccionar Planeta\n2.Seleccionar Nave\n3.Comenzar Viaje\n4.Salir\n");
-        System.out.println("-------------------------------------------");
-        System.out.print("\nSeleccione su opcion de preferencia: ");
-        var selection= sc.nextInt();
-        return selection;        
+
+    // metodo para menu principal
+    public static Integer menu() {
+        boolean ok = false;
+        var selection = 0;
+        // ciclo para la verificar errores        
+        do {
+            System.out.println("\n-MENU-");
+            System.out.println("\n1.Destinos\n2.Naves\n3.Gestionar Viaje\n4.Comenzar Viaje\n5.Salir\n");
+            System.out.println("-------------------------------------------");
+            System.out.print("\nSeleccione su opcion de preferencia: ");
+            try {
+                selection = sc.nextInt();
+                ok = true;
+            } catch (Exception e) {
+                System.out.println("La opción debe ser numerica...");
+                sc.nextLine();
+            }
+        } while (!ok);
+        return selection;
     }
-    // metodo para la seleccion y detalles de planeta
-    public Integer  planetMap(){        
-        System.out.println("-------------------------------------------");
-        System.out.println("-MENU DE PLANETAS-");
-        System.out.println("\n1.Mercurio\nEs el planeta más cercano al Sol y el más pequeño del sistema solar.\nNo tiene atmósfera significativa, lo que provoca temperaturas extremas entre el día y la noche.\nDistancia= 222mill Km");
-        System.out.println("\n2.Venus\nEs el planeta más caliente debido a su densa atmósfera de dióxido de carbono que genera un efecto invernadero extremo.\nTiene una superficie llena de volcanes y montañas.\nDistancia= 261mill Km");
-        System.out.println("\n3.Marte\nConocido como el \\\"planeta rojo\\\" por su superficie cubierta de óxido de hierro.\nTiene el volcán más grande del sistema solar, el Monte Olimpo.\nDistancia= 401mill Km");
-        System.out.println("\n4.Júpiter\nEs el planeta más grande del sistema solar y está compuesto mayormente de hidrógeno y helio.\nTiene una tormenta gigante llamada la Gran Mancha Roja.\nDistancia= 968mill Km");
-        System.out.println("\n5.Saturno\nFamoso por sus impresionantes anillos compuestos de hielo y roca.\nEs el segundo planeta más grande del sistema solar.\nDistancia= 1660mill Km");
-        System.out.println("\n6.Urano\nEs un gigante helado con una atmósfera de metano, lo que le da su característico color azul verdoso.\nRota de lado, con su eje casi perpendicular a su órbita.\nDistancia= 3151mill Km");
-        System.out.println("\n7.Neptuno\nEs el planeta más lejano del Sol y también un gigante helado.\nEs conocido por sus fuertes vientos y tormentas.\nDistancia= 4700mill Km");
-        System.out.println("-------------------------------------------");
-        System.out.print("\nPor favor, seleccione su planeta de destino: ");
-        // variable scanner
-        var selection= sc.nextInt();
+
+    // metodo creacion mapa de planetas
+    public static Map<String, Integer> planetMap() {
         // mapa de planetas
-        Map <String, Integer>  planetMap = new HashMap<>();       
+        Map<String, Integer> planetMap = new HashMap<>();
         planetMap.put("Mercurio", 222);
         planetMap.put("Venus", 261);
         planetMap.put("Marte", 401);
         planetMap.put("Júpiter", 968);
-        planetMap.put("Saturno",1660 );
+        planetMap.put("Saturno", 1660);
         planetMap.put("Urano", 3151);
         planetMap.put("Neptuno", 4700);
-        int planetSel=0;
-        // switch para eleccion de planeta
-        switch (selection) {
-            case 1:                
-                planetSel=planetMap.get("Mercurio");                
-                System.out.println("Usted ha seleccionado a Mercurio como destino");     
-                break;
-            case 2:
-                planetSel=planetMap.get("Venus");
-                System.out.println("Usted ha seleccionado a Venus como destino");     
-                break;  
-            case 3:
-                planetSel=planetMap.get("Marte");
-                System.out.println("Usted ha seleccionado a Marte como destino");    
-                break;  
-            case 4:
-                planetSel=planetMap.get("Júpiter");
-                System.out.println("Usted ha seleccionado a Júpiter como destino");    
-                break;    
-            case 5:
-                planetSel=planetMap.get("Saturno");
-                System.out.println("Usted ha seleccionado a Saturno como destino");    
-                break;  
-            case 6:
-                planetSel=planetMap.get("Urano");
-                System.out.println("Usted ha seleccionado a Urano como destino");    
-                break;  
-            case 7:
-                planetSel=planetMap.get("Neptuno");
-                System.out.println("Usted ha seleccionado a Neptuno como destino");    
-                break;       
-            default:
-                System.out.println("Seleccion invalida");
-                break;
-        }
-        return planetSel;   
+        return planetMap;
     }
-    // metodo para seleccion y detalles de nave
-    public Integer  naveMap(){        
-        System.out.println("-------------------------------------------");
-        System.out.println("-MENU DE NAVES-");
-        System.out.println("\n1.Mack I\nCapacidad=2 Personas\nVelocidad=200mill Km/d");
-        System.out.println("\n2.Mack II\nCapacidad=8 Personas\nVelocidad=140mill Km/d");
-        System.out.println("\n3.Mack III\nCapacidad=14 Personas\nVelocidad=100mill Km/d");
-        System.out.println("-------------------------------------------");
-        System.out.print("\nPor favor, seleccione su nave de preferencia: ");
-        // variable scanner
-        var selection= sc.nextInt();
-        // mapa de naves
-        Map <String, Integer>  naveMap = new HashMap<>();       
-        naveMap.put("Mack I", 200);
-        naveMap.put("Mack II", 140);
-        naveMap.put("Mack III", 100);
-        int naveSel=0;
-        // switch para eleccion de nave
-        switch (selection) {
-            case 1:                
-                naveSel=naveMap.get("Mack I");                
-                System.out.println("Usted ha seleccionado el Mack I");     
-                break;
-            case 2:
-                naveSel=naveMap.get("Mack II");
-                System.out.println("Usted ha seleccionado el Mack II");     
-                break;  
-            case 3:
-                naveSel=naveMap.get("Mack III");
-                System.out.println("Usted ha seleccionado el Mack III");    
-                break;      
-            default:
-                System.out.println("Seleccion invalida");
-                break;
-        }
-        return naveSel;   
-    }
-    // public Map eventRamdon(){
-        
-        
-    //     return eventRamdon;   
-    // }
 
+    // metodo para visualizar detalles de planetas
+    public static void viewPlanet() {
+        // variable scanner
+        var selection = 0;
+        System.out.println("-------------------------------------------");
+        System.out.println("-DETALLES DE DESTINOS-");
+        //bucle de seleccion 
+        do {
+            System.out.println("-------------------------------------------");
+            System.out.println("\n1.Mercurio\n2.Venus\n3.Marte\n4.Júpiter\n5.Saturno\n6.Urano\n7.Neptuno\n8.Volver\n");
+            System.out.println("-------------------------------------------");
+            System.out.print("\nPor favor, seleccione un destino para saber mas detalles: ");
+            selection = sc.nextInt();
+            // switch para eleccion de planeta
+            switch (selection) {
+                case 1 ->
+                    System.out.println("\nMercurio:\nEs el planeta más cercano al Sol y el más pequeño del sistema solar.\nDistancia= 222mill Km");
+                case 2 ->
+                    System.out.println("\nVenus:\nEs el planeta más caliente debido a su densa atmósfera de dióxido de carbono que genera un efecto invernadero extremo.\nDistancia= 261mill Km");
+                case 3 ->
+                    System.out.println("\nMarte:\nConocido como el \\\"planeta rojo\\\" por su superficie cubierta de óxido de hierro.\nDistancia= 401mill Km");
+                case 4 ->
+                    System.out.println("\nJúpiter:\nEs el planeta más grande del sistema solar y está compuesto mayormente de hidrógeno y helio.\nDistancia= 968mill Km");
+                case 5 ->
+                    System.out.println("\nSaturno:\nFamoso por sus impresionantes anillos compuestos de hielo y roca.\nDistancia= 1660mill Km");
+                case 6 ->
+                    System.out.println("\nUrano:\nEs un gigante helado con una atmósfera de metano, lo que le da su característico color azul verdoso.\nDistancia= 3151mill Km");
+                case 7 ->
+                    System.out.println("\nNeptuno:\nEs conocido por sus fuertes vientos y tormentas.\nDistancia= 4700mill Km");
+                case 8 ->
+                    System.out.println("\nUsted ha seleccionado volver");
+                default ->
+                    System.out.println("\nSeleccion invalida");
+            }
+        } while (selection != 8);
+    }
+
+    // metodo para seleccion y detalles de nave
+    public static Map<String, Integer> spaceshipMap() {
+        // mapa de naves
+        Map<String, Integer> spaceshipMap = new HashMap<>();
+        spaceshipMap.put("Mack I", 200);
+        spaceshipMap.put("Mack II", 140);
+        spaceshipMap.put("Mack III", 100);
+        return spaceshipMap;
+    }
+
+    // metodo para visualizar detalles de naves
+    public static void viewSpaceship() {
+        System.out.println("-------------------------------------------");
+        System.out.println("-DETALLES DE NAVES-");
+        // variable scanner
+        var selection = 0;
+        //bucle de seleccion 
+        do {
+            System.out.println("\n1.Mack I\n2.Mack II\n3.Mack III\n4.volver\n");
+            System.out.println("-------------------------------------------");
+            System.out.print("\nPor favor, seleccione su nave de preferencia para saber mas detalles: ");
+            selection = sc.nextInt();
+            switch (selection) {
+                case 1 ->
+                    System.out.println("\nMack I\nCapacidad=2 Personas\nVelocidad=200mill Km/d");
+                case 2 ->
+                    System.out.println("\nMack II\nCapacidad=8 Personas\nVelocidad=140mill Km/d");
+                case 3 ->
+                    System.out.println("\nMack III\nCapacidad=14 Personas\nVelocidad=100mill Km/d");
+                case 4 ->
+                    System.out.println("\nUsted ha seleccionado volver");
+                default ->
+                    System.out.println("\nSeleccion invalida");
+            }
+        } while (selection != 4);
+    }
+
+    // metodo para gestionar viaje
+    public static Integer managerTrip(Map<String, Integer> pla, Map<String, Integer> nav) {
+        Map<String, Integer> planet = pla;
+        Map<String, Integer> spaceship = nav;
+        System.out.println("-------------------------------------------");
+        System.out.println("-GESTOR DE VIAJE-");
+        System.out.println(planet);
+        System.out.println(spaceship);
+        return 1;
+    }
 }
