fdcc8926124a49abb51a89675722422486153c9d
Joe Weider Berbesi Chapeta <joewbch@gmail.com>
2024-11-22 19:53:46 -0500

    bloques try_cash en opciones de seleccion numerica

--- src/App.java
+++ src/App.java
@@ -6,8 +6,10 @@ import java.util.Scanner;
 // simulador de viaje intergalactivo GalaxyExpress
 
 public class App {
+
     // variable scanner global
     static Scanner sc = new Scanner(System.in);
+
     // metodo principal
     public static void main(String[] args) throws Exception {
         // inicio de simulador        
@@ -45,7 +47,7 @@ public class App {
                 System.out.println("La opción debe ser numerica...");
                 sc.nextLine();
             }
-        } while (!ok || select!=5);               
+        } while (!ok || select != 5);
     }
 
     // metodo para menu principal
@@ -87,37 +89,45 @@ public class App {
     public static void viewPlanet() {
         // variable scanner
         var selection = 0;
+        boolean ok = false;
         System.out.println("-------------------------------------------");
         System.out.println("-DETALLES DE DESTINOS-");
         //bucle de seleccion 
         do {
-            System.out.println("-------------------------------------------");
-            System.out.println("\n1.Mercurio\n2.Venus\n3.Marte\n4.Júpiter\n5.Saturno\n6.Urano\n7.Neptuno\n8.Volver\n");
-            System.out.println("-------------------------------------------");
-            System.out.print("\nPor favor, seleccione un destino para saber mas detalles: ");
-            selection = sc.nextInt();
-            // switch para eleccion de planeta
-            switch (selection) {
-                case 1 ->
-                    System.out.println("\nMercurio:\nEs el planeta más cercano al Sol y el más pequeño del sistema solar.\nDistancia= 222mill Km");
-                case 2 ->
-                    System.out.println("\nVenus:\nEs el planeta más caliente debido a su densa atmósfera de dióxido de carbono que genera un efecto invernadero extremo.\nDistancia= 261mill Km");
-                case 3 ->
-                    System.out.println("\nMarte:\nConocido como el \\\"planeta rojo\\\" por su superficie cubierta de óxido de hierro.\nDistancia= 401mill Km");
-                case 4 ->
-                    System.out.println("\nJúpiter:\nEs el planeta más grande del sistema solar y está compuesto mayormente de hidrógeno y helio.\nDistancia= 968mill Km");
-                case 5 ->
-                    System.out.println("\nSaturno:\nFamoso por sus impresionantes anillos compuestos de hielo y roca.\nDistancia= 1660mill Km");
-                case 6 ->
-                    System.out.println("\nUrano:\nEs un gigante helado con una atmósfera de metano, lo que le da su característico color azul verdoso.\nDistancia= 3151mill Km");
-                case 7 ->
-                    System.out.println("\nNeptuno:\nEs conocido por sus fuertes vientos y tormentas.\nDistancia= 4700mill Km");
-                case 8 ->
-                    System.out.println("\nUsted ha seleccionado volver");
-                default ->
-                    System.out.println("\nSeleccion invalida");
+            try {
+                System.out.println("-------------------------------------------");
+                System.out.println("\n1.Mercurio\n2.Venus\n3.Marte\n4.Júpiter\n5.Saturno\n6.Urano\n7.Neptuno\n8.Volver\n");
+                System.out.println("-------------------------------------------");
+                System.out.print("\nPor favor, seleccione un destino para saber mas detalles: ");
+                selection = sc.nextInt();
+                ok = true;
+                // switch para eleccion de planeta
+                switch (selection) {
+                    case 1 ->
+                        System.out.println("\nMercurio:\nEs el planeta más cercano al Sol y el más pequeño del sistema solar.\nDistancia= 222mill Km");
+                    case 2 ->
+                        System.out.println("\nVenus:\nEs el planeta más caliente debido a su densa atmósfera de dióxido de carbono que genera un efecto invernadero extremo.\nDistancia= 261mill Km");
+                    case 3 ->
+                        System.out.println("\nMarte:\nConocido como el \\\"planeta rojo\\\" por su superficie cubierta de óxido de hierro.\nDistancia= 401mill Km");
+                    case 4 ->
+                        System.out.println("\nJúpiter:\nEs el planeta más grande del sistema solar y está compuesto mayormente de hidrógeno y helio.\nDistancia= 968mill Km");
+                    case 5 ->
+                        System.out.println("\nSaturno:\nFamoso por sus impresionantes anillos compuestos de hielo y roca.\nDistancia= 1660mill Km");
+                    case 6 ->
+                        System.out.println("\nUrano:\nEs un gigante helado con una atmósfera de metano, lo que le da su característico color azul verdoso.\nDistancia= 3151mill Km");
+                    case 7 ->
+                        System.out.println("\nNeptuno:\nEs conocido por sus fuertes vientos y tormentas.\nDistancia= 4700mill Km");
+                    case 8 ->
+                        System.out.println("\nUsted ha seleccionado volver");
+                    default ->
+                        System.out.println("\nSeleccion invalida");
+                }
+            } catch (Exception e) {
+                System.out.println("La opción debe ser numerica...");
+                sc.nextLine();
             }
-        } while (selection != 8);
+
+        } while (!ok || selection != 8);
     }
 
     // metodo para seleccion y detalles de nave
@@ -136,25 +146,34 @@ public class App {
         System.out.println("-DETALLES DE NAVES-");
         // variable scanner
         var selection = 0;
+        boolean ok = false;
         //bucle de seleccion 
         do {
-            System.out.println("\n1.Mack I\n2.Mack II\n3.Mack III\n4.volver\n");
-            System.out.println("-------------------------------------------");
-            System.out.print("\nPor favor, seleccione su nave de preferencia para saber mas detalles: ");
-            selection = sc.nextInt();
-            switch (selection) {
-                case 1 ->
-                    System.out.println("\nMack I\nCapacidad=2 Personas\nVelocidad=200mill Km/d");
-                case 2 ->
-                    System.out.println("\nMack II\nCapacidad=8 Personas\nVelocidad=140mill Km/d");
-                case 3 ->
-                    System.out.println("\nMack III\nCapacidad=14 Personas\nVelocidad=100mill Km/d");
-                case 4 ->
-                    System.out.println("\nUsted ha seleccionado volver");
-                default ->
-                    System.out.println("\nSeleccion invalida");
+            try {
+                System.out.println("\n1.Mack I\n2.Mack II\n3.Mack III\n4.volver\n");
+                System.out.println("-------------------------------------------");
+                System.out.print("\nPor favor, seleccione su nave de preferencia para saber mas detalles: ");
+                selection = sc.nextInt();
+                ok = true;
+                switch (selection) {
+                    case 1 ->
+                        System.out.println("\nMack I\nCapacidad=2 Personas\nVelocidad=200mill Km/d");
+                    case 2 ->
+                        System.out.println("\nMack II\nCapacidad=8 Personas\nVelocidad=140mill Km/d");
+                    case 3 ->
+                        System.out.println("\nMack III\nCapacidad=14 Personas\nVelocidad=100mill Km/d");
+                    case 4 ->
+                        System.out.println("\nUsted ha seleccionado volver");
+                    default ->
+                        System.out.println("\nSeleccion invalida");
+                }
+            } catch (Exception e) {
+                System.out.println("La opción debe ser numerica...");
+                sc.nextLine();
             }
-        } while (selection != 4);
+            
+            
+        } while (!ok || selection != 4);
     }
 
     // metodo para gestionar viaje
