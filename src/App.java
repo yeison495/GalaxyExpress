f505a579ed31935ac0a1f8107147509ea6deb543
Yeison Berbesi Chapeta <yeison.berbesi@gmail.com>
2024-11-26 00:03:46 -0500

    Implementación metodo para comenzar el viaje y metodos de eventos aleatorios

--- src/App.java
+++ src/App.java
@@ -9,8 +9,9 @@ public class App {
     // variable scanner global
     static Scanner sc = new Scanner(System.in);
     static boolean ok;
-    static String chosenSpaceship;
-    static String chosenPlanet;
+    static String chosenSpaceship, chosenPlanet, eventRandomKey;
+    static double timeTravel, oxigenoTravel, fuelTravel;
+    static int eventRandomValue;
 
     // metodo principal
     public static void main(String[] args) throws Exception {
@@ -41,7 +42,7 @@ public class App {
                 case 3 ->
                     tripConfiguration(pla, nav);
                 case 4 ->
-                    System.out.println();
+                    starTravel(eve, sol, timeTravel, oxigenoTravel, fuelTravel);
                 case 5 -> {
                     System.out.println("\n*Al infinito y mas allá...BYE*\n");
                     break;
@@ -163,9 +164,9 @@ public class App {
     public static Map<String, Integer> solutionMap() {
         // mapa de naves
         Map<String, Integer> solutionMap = new LinkedHashMap<>();
-        solutionMap.put("Reparar", 20);
-        solutionMap.put("Desvio", 25);
         solutionMap.put("Esperar", 30);
+        solutionMap.put("Desvio", 25);
+        solutionMap.put("Reparar", 20);
         return solutionMap;
     }
 
@@ -208,7 +209,6 @@ public class App {
         Map<String, Integer> spaceship = nav;
         var distance = 0;
         var speed = 0;
-        double timeTravel, oxigenoTravel, fuelTravel;
         var cantPeople = 0;
         // variable de consumo de oxigeno en litros por dia para una persona
         var oxConsPerDay = 550;
@@ -224,8 +224,9 @@ public class App {
         selectSpaceship(nav);
         selectPlanet(pla);
         cantPeople = numberPeople(spaceship, chosenSpaceship);
-        
-        // recorrer el mapa de planetas para encontrar la distancia según el planeta elegido
+
+        // recorrer el mapa de planetas para encontrar la distancia según el planeta
+        // elegido
         for (Map.Entry<String, Integer> planetDistance : planet.entrySet()) {
             if (planetDistance.getKey().equals(chosenPlanet)) {
                 distance = planetDistance.getValue();
@@ -240,13 +241,13 @@ public class App {
         }
 
         // calculo del tiempo de duración del viaje configurado
-        timeTravel = (double)distance/(double)speed;
-        
+        timeTravel = (double) distance / (double) speed;
+
         // calculo de oxigeno según la cantidad de personas para el viaje configurado
-        oxigenoTravel = (oxConsPerDay*cantPeople)*timeTravel;
+        oxigenoTravel = (oxConsPerDay * cantPeople) * timeTravel;
 
         // calculo de combustible para el viaje configurado
-        fuelTravel = fuelConsPerDay*timeTravel;
+        fuelTravel = fuelConsPerDay * timeTravel;
 
         // mostrar información del viaje configurado
         System.out.println("");
@@ -257,16 +258,20 @@ public class App {
         System.out.printf("El planeta destino es: %s\n", chosenPlanet);
         System.out.printf("La nave de preferencia es: %s\n", chosenSpaceship);
         System.out.printf("Su viaje tendra un estimado de %.2f días\n", timeTravel);
-        System.out.printf("La cantidad de combustible para su viaje es de %.2f litros y el oxigeno %.2f litros\n", fuelTravel, oxigenoTravel);
+        System.out.printf("La cantidad de combustible para su viaje es de %.2f litros y el oxigeno %.2f litros\n",
+                fuelTravel, oxigenoTravel);
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println(
+                "Sera redirigido al menú principal para *comenzar su viaje* interestelar, que la fuerza te acompañe =D");
         System.out.println("-----------------------------------------------------------------------");
         System.out.println("");
     }
 
     // Metodo para elegir nave al configurar el viaje
-    public static void selectSpaceship(Map<String, Integer> nav){
+    public static void selectSpaceship(Map<String, Integer> nav) {
         Map<String, Integer> spaceship = nav;
         var optionShip = 0;
-        
+
         ok = false;
         // Ciclo para repetir el menú si la opción es invalida
         do {
@@ -304,7 +309,7 @@ public class App {
     }
 
     // Metodo para elegir planeta al configurar el viaje
-    public static void selectPlanet(Map<String, Integer> pla){
+    public static void selectPlanet(Map<String, Integer> pla) {
         Map<String, Integer> planet = pla;
         var optionPlanet = 0;
         ok = false;
@@ -352,12 +357,12 @@ public class App {
     }
 
     // Metodo para elegir el número de personas según la nave seleccionada
-    public static int numberPeople(Map<String, Integer> spaceship, String nav){
+    public static int numberPeople(Map<String, Integer> spaceship, String nav) {
         Map<String, Integer> ships = spaceship;
         String chosenSpaceship = nav;
         var cantMax = 0;
         var cantPeople = 0;
-        // Se define por nave la cantidad maxima del pasajeros 
+        // Se define por nave la cantidad maxima del pasajeros
         if (nav.equals("Mack I")) {
             cantMax = 2;
         } else if (nav.equals("Mack II")) {
@@ -370,7 +375,8 @@ public class App {
         do {
             try {
                 System.out.println("-----------------------------------------------------------------------");
-                System.out.printf("Recuerde que la cantidad maxima de personas para la nave %S es de %d \n", nav, cantMax);
+                System.out.printf("Recuerde que la cantidad maxima de personas para la nave %S es de %d \n", nav,
+                        cantMax);
                 System.out.print("Para poder planificar su viaje por favor digite la cantidad de personas a viajar: ");
                 cantPeople = sc.nextInt();
                 ok = true;
@@ -383,5 +389,108 @@ public class App {
         } while (cantPeople <= 0 || cantPeople > cantMax || !ok);
 
         return cantPeople;
+    }
+
+    public static void starTravel(Map<String, Integer> eve, Map<String, Integer> sol, Double timeTravel,
+            Double oxigenoTravel, Double fuelTravel) {
+        Map<String, Integer> events = eve;
+        Map<String, Integer> solutions = sol;
+        Double timeT = timeTravel;
+        Double oxigenoT = oxigenoTravel;
+        Double fuelT = fuelTravel;
+        System.out.println(timeT + " " + oxigenoT + " " + fuelT);
+
+        for (int percent = 0; percent <= 100; percent++) {
+            // Crea una barra de progreso con 50 caracteres
+            int widthBar = 50; // Tamaño fijo de la barra
+            int done = percent * widthBar / 100;
+            String bar = "=".repeat(done) + " ".repeat(widthBar - done);
+
+            // Sobrescribe la misma línea con el progreso
+            System.out.print("\r\033[35m Iniciando => [" + bar + "] " + percent + "% \033[0m");
+            if (percent == 35) {
+                eventRandom(events);
+                eventSolution(solutions, eventRandomKey);
+
+            }
+
+            // Espera 0.1 segundo antes de la siguiente actualización
+            try {
+                Thread.sleep(100); // 100 ms = 0.1 segundo
+            } catch (InterruptedException e) {
+                System.out.println("Error en la pausa del hilo: " + e.getMessage());
+            }
+        }
+
+        // Salto de línea al finalizar
+        System.out.println("\n¡Proceso completado!");
+
+    }
+
+    // metodo para mostrar el evento aleatorio
+    public static void eventRandom(Map<String, Integer> eve) {
+        Map<String, Integer> events = eve;
+        // Obtén el tamaño del mapa
+        int size = events.size();
+
+        // Genera un índice aleatorio dentro del rango del tamaño del mapa
+        int randomIndex = (int) (Math.random() * size);
+
+        // Crea un contador para recorrer las entradas del mapa
+        int count = 0;
+
+        // Recorre las entradas del mapa hasta llegar al índice aleatorio
+        for (Map.Entry<String, Integer> eventRandom : events.entrySet()) {
+            if (count == randomIndex) {
+                // System.out.println(eventRandom.getKey());
+                eventRandomKey = eventRandom.getKey();
+                eventRandomValue = eventRandom.getValue();
+                break;
+            }
+            count++;
+        }
+    }
+
+    // metodo para elegir la solución a un evento aleatorio
+    public static void eventSolution(Map<String, Integer> sol, String event) {
+        Map<String, Integer> solutionEvents = sol;
+        String chosenEvent = event;
+        var optionSolution = 0;
+        ok = false;
+        if (!chosenEvent.equals("Sin eventualidades")) {
+            // Ciclo para repetir el menú si la opción es invalida
+            do {
+                System.out.println("");
+                System.out.println("--------------------------------------");
+                System.out.printf("Un evento de improvisto: %s \n", chosenEvent);
+                System.out.println("--------------------------------------");
+                try {
+                    var shipIndex = 1;
+                    for (String solutionsName : solutionEvents.keySet()) {
+                        if (chosenEvent.equals("Falla en la nave")) {
+                            if (!solutionsName.equals("Desvio")) {
+                                System.out.println(shipIndex + ". " + solutionsName);
+                            }
+                        } else if (chosenEvent.equals("Lluvia de asteroides") || chosenEvent.equals("Tormenta solar")) {
+                            if (!solutionsName.equals("Reparar")) {
+                                System.out.println(shipIndex + ". " + solutionsName);
+                            }
+                        }
+                        shipIndex++;
+                    }
+                    System.out.println("-----------------------------------------------------------------------");
+                    System.out.print("Para poder continuar el viaje debe elegir una solución: ");
+                    optionSolution = sc.nextInt();
+                    System.out.println("-----------------------------------------------------------------------");
+                    System.out.println("");
+                    ok = true;
+                } catch (Exception e) {
+                    sc.nextLine();
+                    System.out.println("");
+                    System.out.println("La opción es invalida...");
+                    System.out.println("");
+                }
+            } while (!ok);
+        }
     }
 }
